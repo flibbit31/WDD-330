5057ee789c20d93229892859452372dad80befdf
flibbit31 <flibbit31@yahoo.com>
2022-11-12 21:41:15 -0700

    Week9 added without notes.

---
+++ js/week9.js
@@ -0,0 +1,20 @@
+import { addLinks } from './addLinks.js';
+
+const links = [
+    {
+        label: "Back",
+        url: "../index.html"
+    },
+
+    {
+        label: "Notes",
+        url: "notes.html"
+    },
+
+    {
+        label: "Team Activity",
+        url: "W09 Team Activity/index-START.html"
+    }
+];
+
+addLinks(links);
